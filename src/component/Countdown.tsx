f5c1d9873edd656756013ac4e50a4f9d7d29ba9f
Brian Sanders <bdougsand+cfb@gmail.com>
2020-12-06 16:49:13 -0500

    Countdown clock: fixed NaN bug

--- src/component/Countdown.tsx
+++ src/component/Countdown.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
 
 const Countdown = ({ ms }) => {
+  if (isNaN(ms)) ms = 0;
+
   const minutes = '' + Math.floor(ms/60000);
   const seconds = '' + Math.floor(ms%60000/1000);
 
   return (
     <div className={'countdown-clock' + (ms <= 10000 ? ' imminent' : '' )}>
-      <div className="minutes">{minutes.padStart(2, '0')}</div>
-      <div className="clock-separator">:</div>
-      <div className="seconds">{seconds.padStart(2, '0')}</div>
+      <span className="minutes">{minutes.padStart(2, '0')}</span>
+      <span className="clock-separator">:</span>
+      <span className="seconds">{seconds.padStart(2, '0')}</span>
     </div>
   );
 }
